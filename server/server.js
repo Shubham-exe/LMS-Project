06895ba2489e4285775f49dd070cdb7439ef3560
Shubham Yadav <yadavs250090@gmail.com>
2026-03-02 09:41:23 +0530

    Updated server.js for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -4,22 +4,18 @@ import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
 
-// Initialize Express
 const app = express()
 
-// Connect to Database
-await connectDB()
+// Connect to database
+connectDB()
 
 // Middlewares
 app.use(cors())
+app.use(express.json())
 
 // Routes
 app.get('/', (req, res) => res.send("API Working"))
-app.post('/clerk', express.json(), clerkWebhooks)
+app.post('/clerk', clerkWebhooks)
 
-// Port
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+// Export app for Vercel (no app.listen here)
+export default app
