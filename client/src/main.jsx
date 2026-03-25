83647413b82f21a8c109668a986e9bf209538fba
Shubham Yadav <yadavs250090@gmail.com>
2026-03-25 11:52:31 +0530

    fixed router for github pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AppContextProvider } from './context/AppContext.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from "react-router-dom";
 import { ClerkProvider } from '@clerk/clerk-react'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <ClerkProvider
       publishableKey={PUBLISHABLE_KEY}
       clerkJSUrl="https://cdn.jsdelivr.net/npm/@clerk/clerk-js@5/dist/clerk.browser.js"
@@ -18,5 +18,5 @@ createRoot(document.getElementById('root')).render(
         <App />
       </AppContextProvider>
     </ClerkProvider>
-  </BrowserRouter>
+  </HashRouter>
 )
